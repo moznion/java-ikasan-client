a7b1f9487040057b8ea3e262a10daf597848b4fd
moznion <moznion@gmail.com>
2015-06-15 11:18:03 +0900

    Support SSL

--- src/main/java/net/moznion/ikasanclient/IkasanClient.java
+++ src/main/java/net/moznion/ikasanclient/IkasanClient.java
@@ -10,13 +10,24 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContextBuilder;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,12 +53,13 @@ public class IkasanClient {
         private final String host;
         private int port = 4979;
         private boolean useSSL = false;
+        private boolean verifySSL = false;
 
         public IkasanClientBuilder(String host) {
             this.host = host;
         }
 
-        public IkasanClient build() {
+        public IkasanClient build() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
             return new IkasanClient(this);
         }
     }
@@ -56,13 +68,27 @@ public class IkasanClient {
         return new IkasanClientBuilder(host);
     }
 
-    private IkasanClient(IkasanClientBuilder b) {
+    private IkasanClient(IkasanClientBuilder b) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         this.host = b.host;
         this.port = b.port;
         this.useSSL = b.useSSL;
 
+        RegistryBuilder<ConnectionSocketFactory> registryBuilder = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE);
+
+        if (this.useSSL) {
+            SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+            if (!b.verifySSL) {
+                sslContextBuilder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+            }
+            registryBuilder.register("https", new SSLConnectionSocketFactory(sslContextBuilder.build()));
+        }
+
+        HttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(registryBuilder.build());
+
         this.httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.DEFAULT)
+                .setConnectionManager(connectionManager)
                 .setDefaultHeaders(defaultHeaders)
                 .build();
     }
